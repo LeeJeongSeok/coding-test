39da6631bffbb4fc136c07d4331d6654b880b91b
JeongSeok <ljs0429777@gmail.com>
2023-12-18 11:18:17 +0900

    feat: 프로그래머스 레벨1 - 삼총사 풀이 업로드

---
+++ src/main/java/jeongseok/programmers/lv1/ThreeMusketeers.java
@@ -0,0 +1,29 @@
+package jeongseok.programmers.lv1;
+
+/**
+ * 프로그래머스 - 레벨1 - 삼총사
+ */
+
+public class ThreeMusketeers {
+
+	public int solution(int[] number) {
+		int answer = 0;
+
+		for (int i = 0; i < number.length; i++) {
+			for (int j = i + 1; j < number.length; j++) {
+				for (int k = j + 1; k < number.length; k++) {
+					if (number[i] + number[j] + number[k] == 0) {
+						answer++;
+					}
+				}
+			}
+		}
+		return answer;
+	}
+	public static void main(String[] args) {
+		System.out.println(new ThreeMusketeers().solution(new int[]{-2, 3, 0, 2, -5}));
+		System.out.println(new ThreeMusketeers().solution(new int[]{-3, -2, -1, 0, 1, 2, 3}));
+		System.out.println(new ThreeMusketeers().solution(new int[]{-1, 1, -1, 1}));
+	}
+
+}
