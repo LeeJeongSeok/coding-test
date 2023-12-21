82cc0dd6badbaa9a1df4f13ad58ace92df3ada15
JeongSeok <ljs0429777@gmail.com>
2023-12-22 01:09:04 +0900

    feat: 프로그래머스 레벨1 - 성격 유형 검사하기 성공 풀이 업로드

--- src/main/java/jeongseok/programmers/lv1/PersonalityTypeTester.java
+++ src/main/java/jeongseok/programmers/lv1/PersonalityTypeTester.java
@@ -6,146 +6,73 @@ import java.util.stream.*;
 public class PersonalityTypeTester {
 
 	public String solution(String[] survey, int[] choices) {
-
-		HashMap<String, Integer> first = new HashMap<>();
-		HashMap<String, Integer> second = new HashMap<>();
-		HashMap<String, Integer> third = new HashMap<>();
-		HashMap<String, Integer> forth = new HashMap<>();
-
-		List<String> list = new ArrayList<>();
+		HashMap<String, Integer> map = new HashMap<>();
 		int[] score = new int[]{0, 3, 2, 1, 0, 1, 2, 3};
+		List<String> answer = new ArrayList<>();
 
-		first.put("R", 0);
-		first.put("T", 0);
+		map.put("R", 0);
+		map.put("T", 0);
+		map.put("C", 0);
+		map.put("F", 0);
+		map.put("J", 0);
+		map.put("M", 0);
+		map.put("A", 0);
+		map.put("N", 0);
 
-		second.put("C", 0);
-		second.put("F", 0);
-
-		third.put("J", 0);
-		third.put("M", 0);
-
-		forth.put("A", 0);
-		forth.put("N", 0);
-
-
-		for (int i = 0; i < choices.length; i++) {
-			String[] ans = survey[i].split("");
-			String disagree = ans[0];
-			String agree = ans[1];
+		for(int i = 0; i < choices.length; i++) {
+			String[] str = survey[i].split("");
+			String disagree = str[0];
+			String agree = str[1];
 
 			if (choices[i] >= 1 && choices[i] <= 3) {
-
-				if (first.containsKey(disagree)) {
-					first.put(disagree, score[choices[i]]);
-				}
-
-				if (second.containsKey(disagree)) {
-					second.put(disagree, score[choices[i]]);
-				}
-
-				if (third.containsKey(disagree)) {
-					third.put(disagree, score[choices[i]]);
-				}
-
-				if (forth.containsKey(disagree)) {
-					forth.put(disagree, score[choices[i]]);
-				}
+				map.put(disagree, map.get(disagree) + score[choices[i]]);
 			}
 
 			if (choices[i] >= 5 && choices[i] <= 7) {
-				if (first.containsKey(agree)) {
-					first.put(agree, score[choices[i]]);
-				}
-
-				if (second.containsKey(agree)) {
-					second.put(agree, score[choices[i]]);
-				}
-
-				if (third.containsKey(agree)) {
-					third.put(agree, score[choices[i]]);
-				}
-
-				if (forth.containsKey(agree)) {
-					forth.put(agree, score[choices[i]]);
-				}
+				map.put(agree, map.get(agree) + score[choices[i]]);
 			}
 		}
 
-
-		Integer firstMaxValue = Collections.max(first.values());
-		Integer secondMaxValue = Collections.max(second.values());
-		Integer thirdMaxValue = Collections.max(third.values());
-		Integer forthMaxValue = Collections.max(forth.values());
-
-
-		List<String> firstTemp = new ArrayList<>();
-		for (String key : first.keySet()) {
-			if (first.get(key).equals(firstMaxValue)) {
-				firstTemp.add(key);
-			}
-		}
-
-		// value 값이 동일한 경우 사전순으로 정렬했을 때 가장 앞에 오는 문자가 list에 더해져야한다.
-		if (firstTemp.size() >= 2) {
-			Collections.sort(firstTemp);
-			list.add(firstTemp.get(0));
+		if (Objects.equals(map.get("R"), map.get("T"))) {
+			answer.add("R");
+		} else if (map.get("R") > map.get("T")) {
+			answer.add("R");
 		} else {
-			list.add(firstTemp.get(0));
+			answer.add("T");
 		}
 
-		List<String> secondTemp = new ArrayList<>();
-		for (String key : second.keySet()) {
-			if (second.get(key).equals(secondMaxValue)) {
-				secondTemp.add(key);
-			}
-		}
-
-		if (secondTemp.size() >= 2) {
-			Collections.sort(secondTemp);
-			list.add(secondTemp.get(0));
+		if (Objects.equals(map.get("C"), map.get("F"))) {
+			answer.add("C");
+		} else if (map.get("C") > map.get("F")) {
+			answer.add("C");
 		} else {
-			list.add(secondTemp.get(0));
+			answer.add("F");
 		}
 
-
-		List<String> thirdTemp = new ArrayList<>();
-		for (String key : third.keySet()) {
-			if (third.get(key).equals(thirdMaxValue)) {
-				thirdTemp.add(key);
-			}
-		}
-
-		if (thirdTemp.size() >= 2) {
-			Collections.sort(thirdTemp);
-			list.add(thirdTemp.get(0));
+		if (Objects.equals(map.get("J"), map.get("M"))) {
+			answer.add("J");
+		} else if (map.get("J") > map.get("M")) {
+			answer.add("J");
 		} else {
-			list.add(thirdTemp.get(0));
+			answer.add("M");
 		}
 
-		List<String> forthTemp = new ArrayList<>();
-		for (String key : forth.keySet()) {
-			if (forth.get(key).equals(forthMaxValue)) {
-				forthTemp.add(key);
-			}
-		}
-
-		if (forthTemp.size() >= 2) {
-			Collections.sort(forthTemp);
-			list.add(forthTemp.get(0));
+		if (Objects.equals(map.get("A"), map.get("N"))) {
+			answer.add("A");
+		} else if (map.get("A") > map.get("N")) {
+			answer.add("A");
 		} else {
-			list.add(forthTemp.get(0));
+			answer.add("N");
 		}
 
+		String result = answer.stream().collect(Collectors.joining());
 
-		String answer = list.stream()
-			.collect(Collectors.joining());
-
-		return answer;
+		return result;
 	}
 
 	public static void main(String[] args) {
-//		new PersonalityTypeTester().solution(new String[]{"AN", "CF", "MJ", "RT", "NA"}, new int[]{5, 3, 2, 7, 5});
-//		new PersonalityTypeTester().solution(new String[]{"TR", "RT", "TR"}, new int[]{7, 1, 3});
+		new PersonalityTypeTester().solution(new String[]{"AN", "CF", "MJ", "RT", "NA"}, new int[]{5, 3, 2, 7, 5});
+		new PersonalityTypeTester().solution(new String[]{"TR", "RT", "TR"}, new int[]{7, 1, 3});
 		new PersonalityTypeTester().solution(new String[]{"RT", "TR", "FC", "CF", "MJ", "JM", "AN", "NA"}, new int[]{1});
 	}
 
