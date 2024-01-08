c2fb1b4903f44fd29707aa65e80bb55cdac8fbc5
JeongSeok <ljs0429777@gmail.com>
2024-01-08 20:23:32 +0900

    feat: 프로그래머스 레벨2 - 미로 탈출 성공 풀이 업로드 (2회차 성공)

--- src/main/java/jeongseok/programmers/lv2/MazeEscape.java
+++ src/main/java/jeongseok/programmers/lv2/MazeEscape.java
@@ -5,10 +5,12 @@ import java.util.*;
 class Position {
 	int x;
 	int y;
+	int distance;
 
-	public Position(int x, int y) {
+	public Position(int x, int y, int distance) {
 		this.x = x;
 		this.y = y;
+		this.distance = distance;
 	}
 }
 
@@ -16,109 +18,75 @@ public class MazeEscape {
 
 	int[] dx = new int[]{-1, 0, 1, 0};
 	int[] dy = new int[]{0, 1, 0, -1};
-	boolean[][] visited;
-
-	Queue<Position> leverQueue = new LinkedList<>();
-	Queue<Position> exitQueue = new LinkedList<>();
-
-	Position start = null;
-	Position exit = null;
-	Position lever = null;
 
 	public int solution(String[] maps) {
-		int answer = 0;
-		visited = new boolean[maps.length][maps[0].length()];
 
+		int[] start = findPosition('S', maps);
+		int[] lever = findPosition('L', maps);
+
+		int distanceToLever = bfs('L', start, maps);
+		int distanceToExit = bfs('E', lever, maps);
+
+		if (distanceToLever == 0) {
+			return -1;
+		}
+
+		if (distanceToExit == 0) {
+			return -1;
+		}
+
+		return distanceToLever + distanceToExit;
+	}
+
+	private int[] findPosition(char destination, String[] maps) {
 		for (int i = 0; i < maps.length; i++) {
-			String[] split = maps[i].split("");
-			for (int j = 0; j < split.length; j++) {
-				if (maps[i].charAt(j) == 'S') {
-					start = new Position(i, j);
-				}
-
-				if (maps[i].charAt(j) == 'E') {
-					exit = new Position(i, j);
-				}
-
-				if (maps[i].charAt(j) == 'L') {
-					lever = new Position(i, j);
+			for (int j = 0; j < maps[i].length(); j++) {
+				if (maps[i].charAt(j) == destination) {
+					return new int[]{i, j};
 				}
 			}
 		}
 
-		leverQueue.add(start);
-		visited[start.x][start.y] = true;
-		int distanceToLever = findLever(maps);
-		int distanceToExit = findExit(maps);
+		return new int[]{};
+	}
 
-		answer = (distanceToLever == 0) ? -1 : answer + distanceToLever;
-		answer = (distanceToExit == 0) ? -1 : answer + distanceToExit;
+	private int bfs(char destination, int[] start, String[] maps) {
+		Queue<Position> queue = new LinkedList<>();
+		boolean[][] visited = new boolean[maps.length][maps[0].length()];
+
+		queue.add(new Position(start[0], start[1], 0));
+		visited[start[0]][start[1]] = true;
+
+		int answer = 0;
+
+		while (!queue.isEmpty()) {
+			Position cur = queue.poll();
+
+			if (maps[cur.x].charAt(cur.y) == destination) {
+				answer = cur.distance;
+				break;
+			}
+
+			for (int i = 0; i < 4; i++) {
+				int nx = cur.x + dx[i];
+				int ny = cur.y + dy[i];
+
+				if (nx < 0 || nx >= maps.length || ny < 0 || ny >= maps[0].length()) {
+					continue;
+				}
+
+				if (maps[nx].charAt(ny) == 'X' || visited[nx][ny]) {
+					continue;
+				}
+
+				queue.add(new Position(nx, ny, cur.distance + 1));
+				visited[nx][ny] = true;
+			}
+		}
 
 		return answer;
 	}
 
-	private int findLever(String[] maps) {
-		int distance = 0;
-
-		while (!leverQueue.isEmpty()) {
-			Position cur = leverQueue.poll();
-
-			if (maps[cur.x].charAt(cur.y) == 'L') {
-				exitQueue.add(new Position(cur.x, cur.y));
-				break;
-			}
-
-			for (int i = 0; i < 4; i++) {
-				int nx = cur.x + dx[i];
-				int ny = cur.y + dy[i];
-
-				if (nx < 0 || nx >= maps.length || ny < 0 || ny >= maps[0].length()) {
-					continue;
-				}
-
-				if (maps[nx].charAt(ny) == 'X' || visited[nx][ny]) {
-					continue;
-				}
-
-				leverQueue.add(new Position(nx, ny));
-				visited[nx][ny] = true;
-				distance++;
-			}
-		}
-
-		return distance;
-	}
-
-	private int findExit(String[] maps) {
-		int distance = 0;
-
-		while (!exitQueue.isEmpty()) {
-			Position cur = exitQueue.poll();
-
-			if (maps[cur.x].charAt(cur.y) == 'E') {
-				break;
-			}
-
-			for (int i = 0; i < 4; i++) {
-				int nx = cur.x + dx[i];
-				int ny = cur.y + dy[i];
-
-				if (nx < 0 || nx >= maps.length || ny < 0 || ny >= maps[0].length()) {
-					continue;
-				}
-
-				if (maps[nx].charAt(ny) == 'X' || visited[nx][ny]) {
-					continue;
-				}
-
-				exitQueue.add(new Position(nx, ny));
-				visited[nx][ny] = true;
-				distance++;
-			}
-		}
-
-		return distance;
-	}
 	public static void main(String[] args) {
 		new MazeEscape().solution(new String[]{"SOOOL","XXXXO","OOOOO","OXXXX","OOOOE"});
 		new MazeEscape().solution(new String[]{"LOOXS","OOOOX","OOOOO","OOOOO","EOOOO"});
