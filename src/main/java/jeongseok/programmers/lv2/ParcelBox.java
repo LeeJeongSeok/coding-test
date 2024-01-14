aa2325d52bb426f45f94d59b8111b0db2e20bc9c
JeongSeok <ljs0429777@gmail.com>
2024-01-14 17:30:43 +0900

    feat: 프로그래머스 레벨2 - 택배상자 풀이 업로드

--- src/main/java/jeongseok/programmers/lv2/ParcelBox.java
+++ src/main/java/jeongseok/programmers/lv2/ParcelBox.java
@@ -6,36 +6,33 @@ public class ParcelBox {
 
 	public int solution(int[] order) {
 		int answer = 0;
-
-		int[] arr = new int[order.length];
-		Stack<Integer> main = new Stack<>();
-		Stack<Integer> sub = new Stack<>();
-
-		for (int i = 1; i <= order.length; i++) {
-			arr[i - 1] = i;
-		}
-
-		// 컨테이너 벨트는 1번부터 상자를 내릴 수 있기 때문에 stack에 역순으로 저장
-		for (int i = order.length - 1; i >= 0; i--) {
-			main.push(arr[i]);
-		}
-
 		int index = 0;
-		while (!main.isEmpty()) {
-			if (main.peek() != order[index]) {
-				sub.push(main.pop());
-			} else {
-				main.pop();
+		int currentBox = 1;
+
+		Stack<Integer> stack = new Stack<>();
+
+		while (index < order.length) {
+			if (order[index] > currentBox) {
+				stack.push(currentBox);
+				currentBox++;
+			} else if (order[index] == currentBox) {
 				answer++;
 				index++;
+				currentBox++;
+			} else {
+				while (!stack.isEmpty() && order[index] == stack.peek()) {
+					stack.pop();
+					answer++;
+					index++;
+				}
+				break;
 			}
 		}
-
 		return answer;
 	}
 
 	public static void main(String[] args) {
-		new ParcelBox().solution(new int[]{4, 5, 1, 2, 3});
+		new ParcelBox().solution(new int[]{4, 3, 1, 2, 5});
 		new ParcelBox().solution(new int[]{5, 4, 3, 2, 1});
 	}
 
