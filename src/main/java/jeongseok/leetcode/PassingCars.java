fc39c79cb89c3cfca5332568c96bb11078f98594
JeongSeok <ljs0429777@gmail.com>
2024-02-01 14:31:14 +0900

    feat: 코딜리티 - PassingCars 성공 풀이 업로드

--- src/main/java/jeongseok/leetcode/PassingCars.java
+++ src/main/java/jeongseok/leetcode/PassingCars.java
@@ -2,31 +2,27 @@ package jeongseok.leetcode;
 
 public class PassingCars {
 
+	//https://app.codility.com/demo/results/trainingQ29Y8W-3SJ/ 0보다 작을 떄 고려해야함,,,
 	public int solution(int[] A) {
 		// Implement your solution here
 		int answer = 0;
 		int count = 0;
-		int j = 0;
 
 		for (int i = 0; i < A.length; i++) {
-			if (A[i] == 1 && i < A.length - 1) {
-				continue;
-			}
-			if (A[i] == 0 || i == A.length - 1) {
+			count += A[i];
+		}
+
+		for (int i = 0; i < A.length; i++) {
+			if (A[i] == 0) {
 				answer += count;
-				count = 0;
-
-				j = i;
+			} else {
+				count--;
 			}
+		}
 
-			while (j < A.length) {
-				if (A[j] == 0) {
-					j++;
-				} else {
-					count++;
-					j++;
-				}
-			}
+		// 최악의 경우 음수가 들어오는 경우도 포함되어 있기 때문에 0보다 작은 경우도 포함시켜줘야 한다.
+		if (answer > 1_000_000_000 || answer < 0) {
+			return -1;
 		}
 
 		return answer;
