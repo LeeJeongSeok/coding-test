4402182ca205f1d6788021a1c0b4ef8a9ee1b495
JeongSeok <ljs0429777@gmail.com>
2024-02-21 18:15:00 +0900

    feat: 백준 - 15649 풀이 업로드 (2회차)

---
+++ src/main/java/jeongseok/baekjoon/_15649_2.java
@@ -0,0 +1,51 @@
+package jeongseok.baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class _15649_2 {
+
+	static int N, M;
+	static int[] arr, result;
+	static boolean[] visited;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		arr = new int[N];
+		result = new int[M];
+		visited = new boolean[N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = i + 1;
+		}
+
+		dfs(0);
+
+		System.out.println(sb.toString());
+	}
+
+	private static void dfs(int depth) {
+		if (depth == M) {
+			for (int i = 0; i < result.length; i++) {
+				sb.append(result[i]).append(" ");
+			}
+			sb.append("\n");
+		} else {
+			for (int i = 0; i < N; i++) {
+				if (!visited[i]) {
+					visited[i] = true;
+					result[depth] = arr[i];
+					dfs(depth + 1);
+					visited[i] = false;
+				}
+			}
+		}
+	}
+
+}
