f589ea36617a67a09b2464e078624de8eeb5ac0f
JeongSeok <ljs0429777@gmail.com>
2024-02-21 18:26:36 +0900

    feat: 백준 - 1789 풀이 업로드 (2회차)

--- src/main/java/jeongseok/baekjoon/_1789_2.java
+++ src/main/java/jeongseok/baekjoon/_1789_2.java
@@ -5,8 +5,26 @@ import java.util.*;
 
 public class _1789_2 {
 
+	static long N;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		N = Long.parseLong(br.readLine());
+
+		long sum = 0;
+		long count = 0;
+
+		for (int i = 1; ; i++) {
+			if (sum > N) {
+				break;
+			} else {
+				sum += i;
+				count++;
+			}
+		}
+
+		System.out.println(count - 1);
 	}
 
 }
