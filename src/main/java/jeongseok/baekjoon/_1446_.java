377550d47dc1d1270a904dc01df5a858500e28e6
JeongSeok <ljs0429777@gmail.com>
2024-02-26 18:02:28 +0900

    feat: 백준 - 1446 푸는 중

---
+++ src/main/java/jeongseok/baekjoon/_1446_.java
@@ -0,0 +1,14 @@
+package jeongseok.baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class _1446_ {
+
+	static int N, D;
+
+	public static void main(String[] args) throws Exception {
+
+	}
+
+}
