d28f00abb75197682aad83075ea96e277b765cd3
JeongSeok <ljs0429777@gmail.com>
2024-03-10 19:24:13 +0900

    feat: 백준 - 11478 풀이 업로드 (2회차)

---
+++ src/main/java/jeongseok/baekjoon/_11478_2.java
@@ -0,0 +1,23 @@
+package jeongseok.baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class _11478_2 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String S = br.readLine();
+
+		HashSet<String> set = new HashSet<>();
+
+		for (int i = 0; i < S.length(); i++) {
+			for (int j = i + 1; j <= S.length(); j++) {
+				set.add(S.substring(i, j));
+			}
+		}
+
+		System.out.println(set.size());
+	}
+
+}
